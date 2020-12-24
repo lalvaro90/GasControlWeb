2538147efdd08734baf004119386debfe623e87c
lalvaro90 <lalvaro90@gmail.com>
2020-12-24 12:05:31 -0600

    changes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import {} from 'jasmine'
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,7 +11,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('AssetsApp app is running!');
   });
 
   afterEach(async () => {
